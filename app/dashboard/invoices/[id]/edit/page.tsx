4efdac09e50a817fa45e59ff970b59f39ee4ee59
Alessandro Reis <alessandrodreis23@gmail.com>
2023-11-01 17:05:07 -0300

    chapter 12 - updating: fetch specific invoice

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,10 +1,14 @@
+import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data'
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs'
 import Form from '@/app/ui/invoices/edit-form'
-// import { fetchCustomers } from '@/app/lib/data';
-import { customers } from '@/app/lib/placeholder-data'
 
 export default async function Page({ params }: { params: { id: string } }) {
 	const id = params.id
+	const [invoice, customers] = await Promise.all([
+		fetchInvoiceById(id),
+		fetchCustomers(),
+	])
+
 	return (
 		<main>
 			<Breadcrumbs
@@ -18,7 +22,7 @@ export default async function Page({ params }: { params: { id: string } }) {
 				]}
 			/>
 			<Form
-				invoice={invoice}
+				invoice={invoice!}
 				customers={customers}
 			/>
 		</main>
